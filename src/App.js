e2a65c4351c467ddfdc9fc7a5788c527c636122f
UmerGillani36 <syedomershahgillani.com>
2023-05-01 16:28:14 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import WordColour from './pages/Home/WordColour/WordColour';
 import Frogger from './pages/Frogger/Frogger';
 import FindAWord from './pages/FindAWord/FindAWord';
-
+import WordColour from './pages/WordColour/WordColour'
 function App () {
   return (
     <>
